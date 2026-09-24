e68828c8d24a43a52321daee5e937e5abb682d06
robotsamurai <maedayukimi@icloud.com>
2026-09-24 15:36:41 +0900

    build print_token: find 1st imgsrc url(find only "" quoted. u need to fix to work on '' too)

--- spider.c
+++ spider.c
@@ -3,12 +3,34 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct
 {
 	char*	data;
 	size_t	size;
 }	t_buffer;
+
+bool	print_token(char* src)
+{
+	char*	start = strstr(src, "<img");
+	start+= 4;
+
+	char*	s_start = strstr(start, "src=\"");
+	s_start += 5;
+
+	char*	s_end = strstr(s_start, "\"");
+	size_t	len = s_end - s_start;
+
+	char*	dst = malloc(len + 1);
+	if (!dst)
+		exit(1);
+
+	memcpy(dst, s_start, len);
+	dst[len] = 0;
+
+	printf("%s\n", dst);
+}
 
 size_t	write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
 {
@@ -42,12 +64,11 @@ int	main(void)
 
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
-		curl_easy_setopt(curl, CURLOPT_URL, "https://www.columbia.edu/~fdc/sample.html");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://books.toscrape.com/");
 		curl_easy_setopt(curl, CURLOPT_USERAGENT, "my-spidar/1.0");
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 
-		printf("%s\n", buf.data);
+		print_token(buf.data);
 	}
-
 }
